cb188155db57a3ce8a48d175438502f3ba06872e
Danish <iamdanishk555@gmail.com>
2022-04-11 23:22:22 +0545

    Selected the Brush Size From a Custom Dialog that we created

--- app/src/main/java/com/example/kidsdrawingapp/DrawingView.kt
+++ app/src/main/java/com/example/kidsdrawingapp/DrawingView.kt
@@ -3,23 +3,41 @@ package com.example.kidsdrawingapp
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
+import android.util.TypedValue
 import android.view.MotionEvent
 import android.view.View
 
 
 class DrawingView(context: Context, attrs: AttributeSet ) : View(context, attrs){
+    //Variable of CustomPath inner class
     private var mDrawPath: CustomPath? = null
+    //An instance of the Bitmap
     private var mCanvasBitmap: Bitmap? = null
+    //The Paint class holds the style and color information about how to draw
     private var mDrawPaint: Paint? = null
+    //Instance of Canvas paint view
     private var mCanvasPaint: Paint? = null
+    //Variable for stroke/brush size to draw on the canvas
     private var mBrushSize: Float = 0.toFloat()
+    //Variable to hold a color of the stroke
     private var color = Color.BLACK
+    /**
+     * A variable for canvas, will be initialized later and used
+     *
+     * The Canvas class holds the 'draw' calls. To draw something, you need 4 basic components.
+     * the draw calls(writing into the bitmap), a drawing primitive(e.g. Rect, Path, text, Bitmap),
+     * and a paint(to describe the colors and styles for the drawing)
+     */
     private var canvas: Canvas? = null
     private val mPaths = ArrayList<CustomPath>()
 
     init {
         setUpDrawing()
     }
+
+    /**
+     *This method initializes the attributes of the ViewForDrawing class
+     */
 
     private fun setUpDrawing() {
         mDrawPaint = Paint()
@@ -29,7 +47,7 @@ class DrawingView(context: Context, attrs: AttributeSet ) : View(context, attrs)
         mDrawPaint!!.strokeJoin = Paint.Join.ROUND
         mDrawPaint!!.strokeCap = Paint.Cap.ROUND
         mCanvasPaint = Paint(Paint.DITHER_FLAG)
-        mBrushSize = 20.toFloat()
+        //mBrushSize = 20.toFloat()
     }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
@@ -38,7 +56,9 @@ class DrawingView(context: Context, attrs: AttributeSet ) : View(context, attrs)
         canvas = Canvas(mCanvasBitmap!!)
     }
 
-    //Change Canvas to Canvas? if fails
+    /**
+     * This method is called when a stroke is drawn on the canvas as a part of painting
+    */
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
         canvas.drawBitmap(mCanvasBitmap!!, 0f,0f, mCanvasPaint)
@@ -57,6 +77,10 @@ class DrawingView(context: Context, attrs: AttributeSet ) : View(context, attrs)
             canvas.drawPath(mDrawPath!!, mDrawPaint!!)
         }
     }
+
+    /**
+     * This method as an event listener when a touch event is detected on the device
+     */
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
         val touchX = event?.x
@@ -91,6 +115,12 @@ class DrawingView(context: Context, attrs: AttributeSet ) : View(context, attrs)
         invalidate()
 
         return true
+    }
+
+    fun setSizeForBrush(newSize: Float){
+        mBrushSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, newSize, resources.displayMetrics)
+
+        mDrawPaint!!.strokeWidth = mBrushSize
     }
 
 
