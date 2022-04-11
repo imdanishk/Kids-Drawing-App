e6fef0926d331e8ee9fbb240dc77bc5c92e3674b
Danish <iamdanishk555@gmail.com>
2022-04-11 17:46:49 +0545

    Letting the Lines Persist on the View

--- app/src/main/java/com/example/kidsdrawingapp/DrawingView.kt
+++ app/src/main/java/com/example/kidsdrawingapp/DrawingView.kt
@@ -15,6 +15,7 @@ class DrawingView(context: Context, attrs: AttributeSet ) : View(context, attrs)
     private var mBrushSize: Float = 0.toFloat()
     private var color = Color.BLACK
     private var canvas: Canvas? = null
+    private val mPaths = ArrayList<CustomPath>()
 
     init {
         setUpDrawing()
@@ -41,6 +42,13 @@ class DrawingView(context: Context, attrs: AttributeSet ) : View(context, attrs)
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
         canvas.drawBitmap(mCanvasBitmap!!, 0f,0f, mCanvasPaint)
+
+        for (path in mPaths){
+            //How thick the pen should be
+            mDrawPaint!!.strokeWidth = path.brushThickness
+            mDrawPaint!!.color = path.color
+            canvas.drawPath(path, mDrawPaint!!)
+        }
 
         if (!mDrawPath!!.isEmpty) {
             //How thick the pen should be
@@ -75,6 +83,7 @@ class DrawingView(context: Context, attrs: AttributeSet ) : View(context, attrs)
                 }
             }
             MotionEvent.ACTION_UP -> {
+                mPaths.add(mDrawPath!!)
                 mDrawPath = CustomPath(color, mBrushSize)
             }
             else -> return false
